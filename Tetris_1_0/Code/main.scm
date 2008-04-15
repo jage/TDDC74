f673ea490bee7a93faaf19cb12f7a3af8b8426d9
Johan Eckerstrom <johan@jage.se>
2008-04-15 16:47:22 +0200

    Cleanup and start using the OO-code

--- Tetris_1_0/Code/main.scm
+++ Tetris_1_0/Code/main.scm
@@ -2,14 +2,21 @@
 
 ;;MAIN
 
+
+;; Load all files
 (load "graphics.scm")
+(load "objects/block.scm")
+(load "objects/piece.scm")
+(load "objects/board.scm")
 
-(define (initialize)
-  ;; Drawing canvas
-  #t)
 
-(define (update)
-  #t)
+;; Dummy
+(define (update) #t)
+
+(define (draw)
+  (clear)
+  (send *board* draw-board)
+  (show))
 
 (define window-width 200)
 (define window-height 400)
@@ -18,100 +25,19 @@
 (define board-width 10)  ; pieces
 (define board-height 20) ; pieces
 
-;(define I '((0 1) (0 2) (0 3) (0 4)))
-;(define T '((0 0) (-1 0) (1 0) (0 1)))
-;(define T '((0 0) (0 1) (-1 0) (0 -1)))
-(define S '((0 0) (0 1) (1 1) (-1 0)))
-(define L '((0 0) (0 -1) (0 1) (1 1)))
-(define I '((0 0) (-1 0) (1 0) (2 0)))
-(define activepiece I)
+(define T '((0 1) (-1 0) (0 -1)))
+(define S '((0 1) (1 1) (-1 0)))
+(define L '((0 -1) (0 1) (1 1)))
+(define I '((-1 0) (1 0) (2 0)))
 
-(define (move-piece piece delta-x delta-y)
-  (if (and (not (null? piece)) (move-possible? piece delta-x delta-y))
-      (let* ((block (car piece))
-             (new-x (+ (car block) delta-x))
-             (new-y (+ (cadr block) delta-y)))
-        (begin
-          (set-car! block new-x)
-          (set-cdr! block (list new-y))
-          (move-piece (cdr piece) delta-x delta-y)))))
+(define *board* (make-object board% (cons 10 20)))
+(send *board* add-piece-on-board (make-object piece% 'L L *yellow-brush*))
 
-(define (move-possible? piece delta-x delta-y)
-  (if (null? piece)
-      #t
-      (let* ((block (car piece))
-             (new-x (+ (car block) delta-x))
-             (new-y (+ (cadr block) delta-y)))
-        (if (and (>= new-x 0) (<= new-x (- board-width 1))
-                 (>= new-y 0) (<= new-y (- board-height 1)))
-            (move-possible? (cdr piece) delta-x delta-y)
-            #f))))
-; XXX
-(define (move-possible? . args) #t)
-
-; coordinate (1 2)
-(define (delta-coordinate first second)
-  (list (- (car second) (car first)) (- (cadr second) (cadr first))))
-
-(define (move-block! block coordinates)
-  (set-car! block (+ (car block) (car coordinates)))
-  (set-cdr! block (list (+ (cadr block) (cadr coordinates)))))
-
-; default
-(define rotate-clockwise #t)
-
-(define (rotate-piece! piece)
-  (define (worker piece fix-block)
-    (if (not (null? piece))
-        (begin
-          (let* ((block (car piece))
-                 (placement (delta-coordinate fix-block block)))
-            (if rotate-clockwise
-                ; clockwise
-                (cond
-                  ((equal? '(0 1) placement)
-                   (move-block! block '(-1 -1)))
-                  ((equal? '(0 -1) placement)
-                   (move-block! block '(1 1))) 
-                  ((equal? '(1 0) placement)
-                   (move-block! block '(-1 1)))
-                  ((equal? '(1 1) placement)
-                   (move-block! block '(-2 0))) 
-                  ((equal? '(1 -1) placement)
-                   (move-block! block '(0 2)))  
-                  ((equal? '(-1 0) placement)
-                   (move-block! block '(1 -1)))
-                  ((equal? '(-1 1) placement)
-                   (move-block! block '(0 -2)))  
-                  ((equal? '(-1 -1) placement)
-                   (move-block! block '(2 0)))
-                  ;; Hack to handle the I-piece
-                  ((equal? '(2 0) placement)
-                   (move-block! block '(-2 2)))) ; (0 2)
-                ; !clockvise
-                (cond
-                  ((equal? '(0 1) placement)
-                   (move-block! block '(1 -1))) ; -> (1 0)
-                  ((equal? '(0 -1) placement)
-                   (move-block! block '(-1 1))) ; -> (-1 0)
-                  ((equal? '(1 0) placement)
-                   (move-block! block '(-1 -1))) ; -> (0 -1)
-                  ((equal? '(1 1) placement)
-                   (move-block! block '(0 -2))) ; -> (1 -1)
-                  ((equal? '(1 -1) placement)
-                   (move-block! block '(-2 0))) ; -> (-1 -1) 
-                  ((equal? '(-1 0) placement)
-                   (move-block! block '(1 1))) ; -> (0 1)
-                  ((equal? '(-1 1) placement)
-                   (move-block! block '(2 0)))  ; -> (1 1)
-                  ((equal? '(-1 -1) placement)
-                   (move-block! block '(0 2)))  ; -> (-1 1))
-                  ; Hack to handle the I-piece
-                  ((equal? '(0 2) placement)
-                   (move-block! block '(2 -2))))) ; (2 0) 
-            (display placement)
-            (worker (cdr piece) fix-block)))))
-  (worker piece (car piece)))
+(define (draw-pieces list-of-pieces)
+  (if (not (null? list-of-pieces))
+      (begin
+        (draw-piece (car list-of-pieces))
+        (draw-pieces (cdr list-of-pieces)))))
 
 (define (draw-piece piece)
   (if (not (null? piece))
@@ -122,27 +48,20 @@
 (define (draw-block block)
   (draw-rectangle (* (car block) piece-size) (* (cadr block) piece-size) piece-size piece-size *black-pen* *blue-brush*))
 
-(define (draw)
-  (clear)
-  (draw-piece activepiece)
-  (show))
-
 (define (handle-key-event key)
-  (cond
-    ((eq? key #\space)
-     (display "space")
-     (set! rotate-clockwise (not rotate-clockwise)))
-    ((eq? key 'up) 
-     (display "up\n")
-     (rotate-piece! activepiece))
-    ((eq? key 'down) 
-     (display "down\n")
-     (move-piece activepiece 0 1))
-    ((eq? key 'left)
-     (display "left\n")
-     (move-piece activepiece -1 0))
-    ((eq? key 'right)
-     (display "right\n")
-     (move-piece activepiece 1 0))))
+  (let ((active-piece (send *board* get-active-piece)))
+    (cond
+      ((eq? key 'up) 
+       (display "up\n")
+       (send active-piece rotate))
+      ((eq? key 'down) 
+       (display "down\n")
+       (send *board* move-piece active-piece 0 -1))
+      ((eq? key 'left)
+       (display "left\n")
+       (send *board* move-piece active-piece -1 0))
+      ((eq? key 'right)
+       (display "right\n")
+       (send *board* move-piece active-piece 1 0)))))
 
 (start-loop)
