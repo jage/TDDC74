4de772db320c39186ae4dbd2ca5ada7aeff7aa56
Johan Eckerstr?m <johan@duh.se>
2008-05-13 15:34:08 +0200

    Added: *next-piece*

--- Tetris_1_0/Code/main.scm
+++ Tetris_1_0/Code/main.scm
@@ -18,14 +18,17 @@
 (define pieces (vector I-piece% J-piece% L-piece% O-piece% S-piece% Z-piece% T-piece%))
 
 (define create-random-piece (lambda () (make-object (vector-ref pieces (random 7)))))
-;(define create-random-piece (lambda () (make-object I-piece%)))
+(define *next-piece* (create-random-piece))
+(define (generate-next-piece)
+  (set! *next-piece* (create-random-piece)))
 
 (define *board* (make-object board% (cons 10 20) 20))
 (initiate-graphics)
 
 (define (initialize)
   (send *board* set-player! (make-object player% "Test"))
-  (send *board* add-piece-on-board-default (create-random-piece)))
+  (send *board* add-piece-on-board-default *next-piece*)
+  (generate-next-piece))
 
 ; The new piece code isn't optimal, if one drops down a piece when the 
 ;  counter is at 23, the time to move it sideways will be 1/24 sec ...
@@ -36,16 +39,18 @@
                 (not (send *board* move-piece (send *board* get-active-piece) (cons 0 -1))))
             (begin
               (send *board* clean-up-board)
-              (if (not (send *board* add-piece-on-board-default (create-random-piece)))
+              (if (not (send *board* add-piece-on-board-default *next-piece*))
                   (begin
                     (stop-loop)
-                    (draw-game-over-text))))))))
+                    (draw-game-over-text))
+                  (generate-next-piece)))))))
 
 (define (handle-key-event key)
   (let ((active-piece (send *board* get-active-piece)))
     (cond
       ((eq? key #\p)
-       (if *should-run* (stop-loop) (start-loop)))
+       (if *should-run* (stop-loop) (start-loop))
+       (draw))
       ((eq? key #\q)
        (hide-gui *gui*)
        (stop-loop))
