f4073162c50b0700dbc9f364165b8f91480acacd
Johan Eckerstr?m <johan@duh.se>
2008-05-13 15:32:54 +0200

    Add a status sidebar with the game information

--- Tetris_1_0/Code/graphics_engine.scm
+++ Tetris_1_0/Code/graphics_engine.scm
@@ -1,31 +1,111 @@
-; Lite småfult
+; Height and width for the drawing canvas
+; Space for the board and some information on the left
+;  and a help feature at the bottom
 (define window-height
-  (send *board* units->pixels (send *board* get-height)))
+  (+ 4 (send *board* units->pixels (send *board* get-height))))
 (define window-width
-  (send *board* units->pixels (send *board* get-width)))
+  (+ (send *board* units->pixels 6) (send *board* units->pixels (send *board* get-width))))
 
+; VOID Draw all the graphics, this procedure is called from the main loop
 (define (draw)
   (clear)
   (draw-pieces (send *board* get-pieces))
+  (draw-score)
+  (draw-status)
+  (draw-design)
+  (draw-shadow)
+  (draw-name)
+  (draw-next-piece)
   (show))
 
+; VOID Draw the players name
+(define (draw-name)
+  (draw-text
+       (string-append "Name: " (send (send *board* get-player) get-name))
+       (+ 10 (send *board* units->pixels (send *board* get-width))) 
+       10
+       *black-pen* *black-brush*))
+
+; VOID Draws a preview of the next piece
+(define (draw-next-piece)
+  (for-each
+   (lambda (block)
+     (draw-block 
+      (cons (+ (send *board* get-width) (send block get-rel-x) 2)
+            (- (send *board* get-height) (send block get-rel-y) 6))
+      (send *next-piece* get-brush)))
+   (send *next-piece* get-blocks)))
+
+; VOID Draw the game status
+(define (draw-status)
+  (if (not *should-run*)
+      (draw-text
+       "Paused!"
+       (+ 10 (send *board* units->pixels (send *board* get-width))) 
+       40
+       *black-pen* *black-brush*)))
+
+; VOID Should check that the x-values are unique
+(define (draw-shadow)
+  (let ((x-coords '()))
+    (for-each 
+     (lambda (block)
+       (set! x-coords (append x-coords (list (send block get-abs-x)))))
+     (send (send *board* get-active-piece) get-blocks))
+    (for-each
+     (lambda (coord)
+       (draw-line 
+        (send *board* units->pixels coord)
+        (- window-height 2)
+        (send *board* get-pixels-per-unit)
+        0
+        *black-pen* *black-brush*))
+     x-coords)))
+
+; VOID
+; Draws a line on the right of the board,
+; on the right side of the line there's some game info
+(define (draw-design)
+  (draw-line 
+   (send *board* units->pixels (send *board* get-width)) 
+   10
+   0
+   window-height
+   *black-pen* *black-brush*))
+
+; VOID Draw the current score
+(define (draw-score)
+  (draw-text
+   (string-append "Score: "(number->string (send (send *board* get-player) get-score)))
+   (+ 10 (send *board* units->pixels (send *board* get-width))) 
+   25
+   *black-pen* *black-brush*))
+
+; VOID draws pieces 
+; <- list-of-pieces [list piece%]
 (define (draw-pieces list-of-pieces)
   (for-each
    (lambda (piece)
      (draw-piece piece))
    list-of-pieces))
 
+; VOID draw pieces
+; <- piece [piece%]
 (define (draw-piece piece)
   (for-each
    (lambda (block)
      (draw-block (send block get-abs-coord) (send piece get-brush)))
    (send piece get-blocks)))
 
+; VOID
+; <- block-coord [cons]
+; <- brush [brush]
 (define (draw-block block-coord brush)
   (draw-rectangle 
    (* (car block-coord) (send *board* get-pixels-per-unit)) 
    (* (- (send *board* get-height) (cdr block-coord) 1) (send *board* get-pixels-per-unit))
    (send *board* get-pixels-per-unit) (send *board* get-pixels-per-unit) *black-pen* brush)
+  ; DEBUG
   (if *debug*
       (draw-text
        (string-append
