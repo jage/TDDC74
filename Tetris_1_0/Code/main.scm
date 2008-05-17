c790e42874f23ef64a5c8eb9eb5f04f9bc1b2578
Johan Eckerstr?m <johan@duh.se>
2008-05-17 14:21:35 +0200

    More cleanup

--- Tetris_1_0/Code/main.scm
+++ Tetris_1_0/Code/main.scm
@@ -27,15 +27,13 @@
       (begin
         (send *board* clean-up-board)
         (if (not (send *board* add-piece-on-board-default (send *supervisor* get-next-piece)))
-            (begin
-              (display "Game over!")
-              (newline)
-              (send *supervisor* stop))
+            (send *supervisor* stop)
             (send *supervisor* generate-next-piece)))))
 
 (define (handle-key-event key)
   ;; Block if game is paused, unless the key is for unpausing, reset or quit
-  (if (or (not (send *supervisor* paused?)) (eq? key #\p) (eq? key #\r) (eq? key #\q))
+  (if (or (not (send *supervisor* paused?))
+          (eq? key #\p) (eq? key #\r) (eq? key #\q))
       (let ((active-piece (send *board* get-active-piece)))
         (cond
           ((eq? key #\i)
